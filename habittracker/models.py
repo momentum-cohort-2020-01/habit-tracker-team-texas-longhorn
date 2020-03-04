1a5f57fccdc6c35cc78c9ca60397546d2f912e69
Ben Prothe <beprothe@gmail.com>
2020-03-04 15:47:46 -0500

    django-extensions and  shell_plus

--- habittracker/models.py
+++ habittracker/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
-class User(models.Model):
-    record = models.ForeignKey(
-        "Record", related_name="user", on_delete=models.CASCADE)
-
-
 class Habit(models.Model):
     name = models.CharField(max_length=60)
     goal_nbr = models.IntegerField(default=0)
@@ -28,5 +22,7 @@ class Activity(models.Model):
 
 
 class Record(models.Model):
+    user = models.ForeignKey(
+        "User", related_name="record", on_delete=models.CASCADE)
     def __str__(self):
         return f'Record: {self.habit, self.activity}'
