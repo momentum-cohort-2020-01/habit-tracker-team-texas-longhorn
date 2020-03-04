090ff65fc42ad3f66408feb86063039e955a68be
Ben Prothe <beprothe@gmail.com>
2020-03-04 18:04:09 -0500

    got registration and admin working

--- habittracker/views.py
+++ habittracker/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Habit, Record, Activity
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def homepage(request):
     return render(request, 'habittracker/index.html')
